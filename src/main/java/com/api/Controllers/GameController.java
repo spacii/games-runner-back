495e7db630a963a01044abe235b10c385e9be56c
Alexander Smirnov <lordmazilka@gmail.com>
2021-02-28 23:10:02 +0700

    make it better

--- src/main/java/com/api/Controllers/GameController.java
+++ src/main/java/com/api/Controllers/GameController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 //@CrossOrigin(origins = "https://games-runner-front-end.herokuapp.com/")
 
-
-
 @CrossOrigin(origins = "https://games-runner-front-end.herokuapp.com/")
 public class GameController {
 
