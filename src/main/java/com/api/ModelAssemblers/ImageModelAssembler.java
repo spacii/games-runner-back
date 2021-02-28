02faa556cbeef60a8df8bc72520543a67d2bbf81
Alexander Smirnov <lordmazilka@gmail.com>
2021-02-28 20:39:24 +0700

    make it better

--- src/main/java/com/api/ModelAssemblers/ImageModelAssembler.java
+++ src/main/java/com/api/ModelAssemblers/ImageModelAssembler.java
@@ -4,6 +4,7 @@ import com.api.Controllers.ImageController;
 import com.api.Entities.Image;
 import com.api.resources.ResourcesController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class ImageModelAssembler implements RepresentationModelAssembler<Image, EntityModel<Image>> {
     @Override
     public EntityModel<Image> toModel(Image image) {
+        Link link = Link.valueOf("https://games-runner-media.s3.us-east-2.amazonaws.com/"+image.getImageType()+"/"+ image.getImageName() + "." + image.getImageFormat());
         return EntityModel.of(
                 image,
                 linkTo(methodOn(ImageController.class).getImageById(image.getImageId())).withSelfRel(),
                 linkTo(methodOn(ImageController.class).getImages()).withRel("images"),
-                linkTo(methodOn(ImageController.class).getImageByIdGame(image.getImageId())).withRel("game")
+                linkTo(methodOn(ImageController.class).getImageByIdGame(image.getImageId())).withRel("game"),
+                link
                 //linkTo(methodOn(ResourcesController.class).getImage(image.getImageType(), image.getImageFormat(), image.getImageName())).withRel("resource")
                 //linkTo("https://games-runner-media.s3.us-east-2.amazonaws.com/"+image.getImageType()+"/"+ image.getImageName() +"." + image.getImageFormat()).withRel("resource")
         );
